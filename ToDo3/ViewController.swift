83ad28089e3a5767efc5a9d72ea1f838a392a07a
Greg Alton <gregaltonmail@gmail.com>
2019-08-13 20:24:03 -0500

    Initial Commit

---
+++ ToDo3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo3
+//
+//  Created by Greg Alton on 8/13/19.
+//  Copyright © 2019 Greg Alton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDo3Tests/ToDo3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDo3Tests.swift
+//  ToDo3Tests
+//
+//  Created by Greg Alton on 8/13/19.
+//  Copyright © 2019 Greg Alton. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo3
+
+class ToDo3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
